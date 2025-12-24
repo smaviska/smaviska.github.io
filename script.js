e33efdd0c867088fbbe6b62554d62e9ca32b73c3
smaviska <massantosa76@gmail.com>
2025-12-25 04:57:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -47,3 +47,17 @@ window.addEventListener("load", () => {
     splash.classList.add("hide");
   }, 1800);
 });
+
+//apa ini
+document.addEventListener("DOMContentLoaded", () => {
+  const hero = document.querySelector(".hero");
+  hero.style.opacity = "0";
+  hero.style.transform = "translateY(-10px)";
+
+  setTimeout(() => {
+    hero.style.transition = "all .6s ease";
+    hero.style.opacity = "1";
+    hero.style.transform = "translateY(0)";
+  }, 100);
+});
+
